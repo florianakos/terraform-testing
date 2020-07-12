fbccedacd785e839636269afe3311df03786cbab
Florian Szabo <florian.akos.szabo@gmail.com>
2020-07-12 18:04:53 +0200

    code cleanup

--- test/go/terraform_test.go
+++ test/go/terraform_test.go
@@ -11,12 +11,9 @@ import (
 )
 
 func TestTerraform(t *testing.T) {
-	// Run the test in parallel
-	t.Parallel()
-	// Generate a unique name for the resource to avoid name colisson
+	// some initial config setup
 	roleName := fmt.Sprintf("developer-role-test-%s", strings.ToLower(random.UniqueId()))
 	bucketName := fmt.Sprintf("developer-bucket-test-%s", strings.ToLower(random.UniqueId()))
-	// some config options for Terraform
 	terraformOptions := &terraform.Options{
 		TerraformDir: "../terraform/aws",
 		Vars: map[string]interface{}{
@@ -28,11 +25,11 @@ func TestTerraform(t *testing.T) {
 		},
 		NoColor: false,
 	}
-	// deferred Terraform DESTROY to clean up after test
+	// deferred Terraform DESTROY to clean up afterwards
 	defer terraform.Destroy(t, terraformOptions)
-	// Terraform apply to create resources
+	// Terraform Init and Apply
 	terraform.InitAndApply(t, terraformOptions)
-	// Check if the outputs from resource creation matches expected output
+	// Assert that the outputs from resource creation matches expected output
 	assert.Equal(t, roleName, terraform.Output(t, terraformOptions, "iam_role_name"))
 	assert.Equal(t, bucketName, terraform.Output(t, terraformOptions, "s3_bucket_name"))
 }
