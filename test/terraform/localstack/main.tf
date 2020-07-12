9719b906c7ed16a3ceb524f15872507ff601e5ce
Florian Szabo <florian.akos.szabo@gmail.com>
2020-07-12 17:44:50 +0200

    small changes

--- test/terraform/localstack/main.tf
+++ test/terraform/localstack/main.tf
@@ -2,28 +2,6 @@ variable "role_name" {
   default = "developer-role"
 }
 
-provider "aws" {
-  access_key                  = "mock"
-  secret_key                  = "mock"
-  region                      = "eu-central-1"
-  s3_force_path_style         = true
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
-
-  endpoints {
-    s3  = "http://localhost:4572"
-    kms = "http://localhost:4599"
-    iam = "http://localhost:4593"
-  }
-}
-
-terraform {
-  backend "local" {
-    path = "./tfstate/terraform.tfstate"
-  }
-}
-
 locals {
   account_id   = "000000000000"
   list_buckets = [aws_s3_bucket.b.id]
